506a39a1590e19ac2398c55a82c0366259a5439d
ZMJZMJ000 <15861669950@163.com>
2024-12-07 18:11:27 +0800

    1

--- src/views/exhibitsOfCulture/exhibitsOfCulture_home.vue
+++ src/views/exhibitsOfCulture/exhibitsOfCulture_home.vue
@@ -5,13 +5,12 @@
         <imgTab />
         <center-content></center-content>
         <!-- 悬浮图标 -->
-    <div class="point" ref="point"
-        :style="`width:${divWidth}px;height:${divHeight}px;left:${moveLeft}px;top:${moveTop}px;`"
-        @touchstart.prevent="(e) => { dragStart(e) }" @touchend.prevent="(e) => { dragEnd(e) }"
-        @touchmove.prevent="(e) => dragMove(e)"
-        @click="to_chatbro">
-        <img class="imgzhushou" src="../../assets/zhushou.png" alt="">
-    </div>
+        <div class="point" ref="point"
+            :style="`width:${divWidth}px;height:${divHeight}px;left:${moveLeft}px;top:${moveTop}px;`"
+            @touchstart.prevent="(e) => { dragStart(e) }" @touchend.prevent="(e) => { dragEnd(e) }"
+            @touchmove.prevent="(e) => dragMove(e)" @click="to_chatbro">
+            <img class="imgzhushou" src="../../assets/zhushou.png" alt="">
+        </div>
     </div>
 
 </template>
@@ -21,40 +20,40 @@ import headerBar from '../../components/exhibitsOfCulture/exhibitsOfCulture_head
 // import footerBar from '../../components/exhibitsOfCulture/exhibitsOfCulture_footer.vue'
 import imgTab from '../../components/exhibitsOfCulture/exhibitsOfCulture_imgTab.vue'
 import CenterContent from '../../components/exhibitsOfCulture/exhibitsOfCulture_center.vue'
-export default{
-    data(){
+export default {
+    data() {
         return {
-            border_distance: 10,//可拖拽元素距离屏幕边界最小距离
-            divWidth: 100,//可拖拽元素宽度
-            divHeight: 100,//可拖拽元素高度
-            moveLeft: 0,//可拖拽元素距离屏幕左边界
-            moveTop: 0,//可拖拽元素距离屏幕上边界
-            clientWidth: window.screen.availWidth,//屏幕宽度
-            clientHeight: window.screen.availHeight//屏幕高度
+            // border_distance: 10,//可拖拽元素距离屏幕边界最小距离
+            // divWidth: 100,//可拖拽元素宽度
+            // divHeight: 100,//可拖拽元素高度
+            // moveLeft: 0,//可拖拽元素距离屏幕左边界
+            // moveTop: 0,//可拖拽元素距离屏幕上边界
+            // clientWidth: window.screen.availWidth,//屏幕宽度
+            // clientHeight: window.screen.availHeight//屏幕高度
         }
     },
     methods: {
-        to_chatbro(){
+        to_chatbro() {
             this.$router.push('/chatbro');
         },
-        dragStart() {
-            this.$refs.point.style.transition = "none";
-        },
-        dragEnd() {
-            this.$refs.point.style.transition = 'all 0.3s';
-            if (this.moveLeft > this.clientWidth / 2) {//若移动位于屏幕的左边或右边，则自动贴合左边或右边（此根据业务具体需求表现而定）
-                this.moveLeft = this.clientWidth - this.divWidth - this.border_distance;
-            } else {
-                this.moveLeft = this.border_distance;
-            }
-        },
-        dragMove(e) {
-            if (e.touches.length) {
-                let touch = e.touches[0];
-                this.moveLeft = touch.clientX - this.divWidth / 2;//因拖动的中间点为Dom节点的中心点，所以left需减去Dom节点一半的宽度
-                this.moveTop = touch.clientY - this.divHeight / 2;//top原理同上
-            }
-        }
+        // dragStart() {
+        //     this.$refs.point.style.transition = "none";
+        // },
+        // dragEnd() {
+        //     this.$refs.point.style.transition = 'all 0.3s';
+        //     if (this.moveLeft > this.clientWidth / 2) {//若移动位于屏幕的左边或右边，则自动贴合左边或右边（此根据业务具体需求表现而定）
+        //         this.moveLeft = this.clientWidth - this.divWidth - this.border_distance;
+        //     } else {
+        //         this.moveLeft = this.border_distance;
+        //     }
+        // },
+        // dragMove(e) {
+        //     if (e.touches.length) {
+        //         let touch = e.touches[0];
+        //         this.moveLeft = touch.clientX - this.divWidth / 2;//因拖动的中间点为Dom节点的中心点，所以left需减去Dom节点一半的宽度
+        //         this.moveTop = touch.clientY - this.divHeight / 2;//top原理同上
+        //     }
+        // }
     },
 
     components: {
@@ -71,12 +70,17 @@ export default{
     width: 80%;
     height: 80%;
 }
-.out {
-    height: 100%;
-    position: relative;
-}
 
 .point {
-    position: absolute;
+    position: fixed;
+    /* 固定位置 */
+    bottom: 5%;
+    /* 下边距 */
+    right: 1%;
+    /* 右边距 */
+    padding: 0;
+    z-index: 10000;
+    /* 设置 z-index 确保悬浮在顶层 */
 }
+
 </style>
