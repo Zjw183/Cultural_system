d63276d2c53276af59756c73d0d48ba92b9060fc
zjw183 <2772642839@qq.com>
2024-12-07 16:26:59 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = true
+Vue.use(ElementUI);
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
 
+// 导入组件库
 import ElementUI from 'element-ui';
+// 导入组件相关样式
 import 'element-ui/lib/theme-chalk/index.css';
- 
-Vue.use(ElementUI);
+// 配置 Vue 插件
+
+
